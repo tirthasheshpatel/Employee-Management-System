ef2ee9ee74377f94254a15755aac953dfa829793
tirthasheshpatel <tirthasheshpatel@gmail.com>
2019-11-05 02:16:08 +0530

    Interface for Employee

---
+++ src/com/EMS/_abstract/BaseEmployee.java
@@ -0,0 +1,28 @@
+package com.EMS._abstract;
+
+import java.util.*;
+import com.EMS.core.Organization;
+
+public interface BaseEmployee
+{
+    public String getOccupation();
+    public void setOccupation(String occupation);
+    public String getName();
+    public void setName(String name);
+    public Organization getOrg();
+    public void setOrg(Organization org);
+    public String getId();
+    public void setId(String id);
+    public String getAccount();
+    public void setAccount(String account);
+    public String getAddress();
+    public void setAddress(String address);
+    public double getSalary();
+    public void setSalary(double salary);
+    public double getBonus();
+    public void setBonus(double bonus);
+    public double getTotal();
+    public boolean getStatus();
+    public void setStatus(boolean status);
+    public void pPrint(boolean print_critical_info, boolean print_org_det);
+}
